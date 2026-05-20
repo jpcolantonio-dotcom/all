f55d5842486b3f29fcfd0b430d0fe67e96c11fe4
jpcolantonio-dotcom <jpcolantonio@gmail.com>
2026-05-19 22:37:46 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://quqtceabwkmmruiqwlg.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_aNfXB2LzYxx_krHg0MnKTQ_zSTgQ...'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
